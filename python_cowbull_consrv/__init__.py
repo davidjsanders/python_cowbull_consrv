1008d68eba05bda74204387fa9ad1d5ad63cd081
David Sanders <dsanderscanada@gmail.com>
2017-12-04 19:47:51 -0500

    Modify configuration loading

--- python_cowbull_consrv/__init__.py
+++ python_cowbull_consrv/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from python_cowbull_consrv.load_env import load_env
+from python_cowbull_consrv.Configurator import Configurator
 
 app = Flask(__name__)
-load_env(app)
+c = Configurator()
+c.execute_load(app=app)
+#load_env(app)
